76f000678baa0d42d1204e09f25e30de410e86f0
root <jannyfidelica@gmail.com>
2022-04-25 21:07:03 +0300

    avant synchro des BDD

--- component/login/login.php
+++ component/login/login.php
@@ -4,13 +4,12 @@ include("./../../component/sing/sing.html");
 <link rel="stylesheet" href="./../../component/login/login.scss"/>
 <div class="login">
     <form methode="post" action="./../../traitement/mail.php">
-        <h1>Start with Joov'Teck</h1>
+        <h1 class="start">Start with Joov'Teck</h1>
         <br/>
-        <label for="mail">E-mail</label>
+        <label class="labemail" for="mail">E-mail</label>
         <br/>
         <input type="mail" name="mail" id="mail" class="mail" required/>
         <br/>
-        <button type="submit">Next</button>
+        <button class"next" type="submit">Next</button>
     </form>
-    <div class="espace"></div>
 </div>
