e5aa4ec58c31746a6bcf29487d43b50e04beabe4
Jeanny Fidelica <93310908+bouboule7@users.noreply.github.com>
2022-04-30 08:00:50 +0300

    Update initialisation.php

--- initialisation.php
+++ initialisation.php
@@ -1,6 +1,6 @@
 <?php
 session_start();//ceci est a fin de pouvoir utiliser des variables de sessions globales dans tout l'appli
-    if($_SESSION['statue']!=1)
+    if($_SESSION['statue']=!1)
         $_SESSION['statue']=0;
     header('Location: ./page/accueil/accueil.php');
 ?>
